749a94cfffb992c68c887b1c177086bc0a5ef5e7
632633533@qq.com <quaff,lyu@gmail.com>
2020-10-27 21:44:22 -0700

    change file location

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 10/27/2020 21:44
+# @Author  : Haoyu Lyu
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ obj/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 10/27/2020 21:43
+# @Author  : Haoyu Lyu
+# @File    : __init__.py.py
+# @Software: PyCharm
