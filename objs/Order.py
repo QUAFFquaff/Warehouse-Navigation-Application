23f9fbf8d2c7d782be4249f91dd4d9754824d2d3
632633533@qq.com <quaff,lyu@gmail.com>
2020-10-28 10:46:14 -0700

    merge

--- objs/Order.py
+++ objs/Order.py
@@ -19,9 +19,29 @@ class Order:
         self.start_time = start_time
         self.finish_time = finish_time
 
+        self.start_time = start_time
+        self.finish_flag = False
+        self.product_list = None
+        self.finish_time = None
+
     def init_products(self,num,all_products,max=math.inf):
         ids = random_int_list(0,len(all_products),num)
         self.products = all_products[ids]
 
+
+
+    def set_product_list(self, product_list):
+        self.product_list = product_list
+
+    def set_finish_time(self, finish_time):
+        self.finish_time = finish_time
+
+    # retrieve order info #
+    def order_info(self):
+        if not self.finish_flag:
+            print('This order has %d items, created at %s, Unfinished' % (len(self.product_list), self.start_time))
+        else:
+            print('This order has %d items, created at %s, finished at %s' % (len(self.product_list), self.start_time,
+                                                                              self.finish_time))
 
 
