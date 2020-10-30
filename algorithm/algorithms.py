f233966883b3c5f562b075a2239987971162e84e
632633533@qq.com <quaff,lyu@gmail.com>
2020-10-30 00:14:11 -0700

    add demo for generate path

--- algorithm/algorithms.py
+++ algorithm/algorithms.py
@@ -5,6 +5,20 @@
 # @File    : algorithms.py
 # @Software: PyCharm
 
-def brute_force(x_matrix,y_matrix,d_matrix,source,target)->list:
-    
-    
+
+
+def brute_force(x_matrix,y_matrix,d_matrix,source,target):
+    '''
+
+    :param x_matrix: x difference
+    :param y_matrix: y-dimensional difference
+    :param d_matrix: x+y
+    :param source: start point
+    :param target: target point, usually source and target are the same
+    :return: result list
+    '''
+    res = []
+
+    return res
+
+
