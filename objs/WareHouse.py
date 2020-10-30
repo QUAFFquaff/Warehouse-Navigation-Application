003e17bc51aa22ebb03f31d036857bce0af74eb0
632633533@qq.com <quaff,lyu@gmail.com>
2020-10-29 23:50:34 -0700

    delete venv

--- objs/WareHouse.py
+++ objs/WareHouse.py
@@ -1,8 +1,8 @@
 
 import sys
-from .DataHandler import DataHandler
-from .Order import Order
-from .Products import Product
+from DataHandler import DataHandler
+from Order import Order
+from Products import Product
 import time
 class WareHouse:
     def __init__(self,worker,orders = [], dhandler = None):
@@ -13,7 +13,7 @@ class WareHouse:
         self.products = []
 
     def set_dhandler(self):
-        self.dhandler =DataHandler()
+        self.dhandler = DataHandler()
 
     def set_orders(self):
         self.orders = Order(time.time())
@@ -28,3 +28,11 @@ class WareHouse:
 
     def set_rules(self):
         pass
+
+    def load_data(self,path):
+        data = self.dhandler.loadTxt(path)
+        for d in data:
+            product = Product(d[0],d[1],d[2])
+            self.products.append(product)
+    
+    
---
+++ objs/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 10/29/2020 23:33
+# @Author  : Haoyu Lyu
+# @File    : test.py
+# @Software: PyCharm
+
+import sys
+from DataHandler import DataHandler
+from WareHouse import WareHouse
+wh = WareHouse()
+wh.load
