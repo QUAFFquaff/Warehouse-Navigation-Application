63607c4a3c4da26177576677b04c9084d10a1a15
632633533@qq.com <quaff,lyu@gmail.com>
2020-10-30 21:22:07 -0700

    change images location

--- UI/main_window.py
+++ UI/main_window.py
@@ -119,7 +119,8 @@ class Main_UI(QMainWindow):
         #TODO: generate graph image
         print('loading file location: ',filename)
         self.wh.load_data(filename)
-        img_name = "data/images/graph_demo.png"
+        print('load finish')
+        img_name = "../data/images/graph_demo.png"
         img = QPixmap(img_name).scaled(self.label_graph.width(), self.label_graph.height())
         self.label_graph.setPixmap(img)
 
--- objs/test.py
+++ objs/test.py
@@ -7,10 +7,10 @@
 
 import sys
 from DataHandler import DataHandler
-from WareHouse import WareHouse
-wh = WareHouse()
+from WareHouse import *
+wh = WareHouse.WareHouse()
 wh.set_dhandler()
-wh.load_data('../data/qvBox-warehouse-data-f20-v01.txt')
+wh.load_data('F:/Users/lenovo/Documents/!ProgramZone/Python/EECS221c/Warehouse-Navigation-Application/data/qvBox-warehouse-data-f20-v01.txt')
 wh.add_order(3)
 for o in wh.orders:
     for p in o.products:
