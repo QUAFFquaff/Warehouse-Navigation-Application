8ad9489417b02059512a11bcb3d5017f168cd356
Ty Z <45839386+tyzhao0931@users.noreply.github.com>
2020-11-04 20:37:06 -0800

    Update BruteForce.py

--- algorithm/BruteForce.py
+++ algorithm/BruteForce.py
@@ -1,57 +1,41 @@
-import numpy as np
 import itertools
 
-def brute_force(x_matrix,y_matrix,d_matrix,source,target,plist)->dict:
 
+def brute_force(m, d_matrix, source, target, plist):
     """
     :param plist: product list
-    :param x_matrix: x difference
-    :param y_matrix: y-dimensional difference
+    :param m: subprocess output message
     :param d_matrix: x+y
     :param source: start point
     :param target: target point, usually source and target are the same
-    :return: result dict
     """
 
-    dlist = []
-    perm = list(itertools.permutations(plist, len(plist)))
-    #print(perm)
+    perm = itertools.permutations(plist, len(plist))
+    # print(perm)
+    # print(len(perm))
 
     temp1 = plist[:]
-    temp1.insert(0,source)
+    temp1.insert(0, source)
     temp1.append(target)
-    #print('!',plist)
-    #print('?',temp1)
 
-    for i in range(len(perm)):
+    min_dist = float('Inf')
+
+    for item in perm:
         d = 0
-        temp = list(perm[i])
+        temp = list(item)
 
         temp.insert(0, source)
         temp.append(target)
-        #print(temp)
+        # print(temp)
+        #print(item)
 
-        for j in range(len(temp)-1):
-            #print(temp[j])
-            d = d+d_matrix[temp1.index(temp[j])][temp1.index(temp[j+1])]
-            #print(temp1.index(temp[j]),temp1.index(temp[j+1]))
-            #print(d)
-        dlist.append(d)
-
-    #print('possible distances: ',dlist)
-    #print(perm[1])
-    index_min = int(np.argmin(dlist))
-    path_min = min(dlist)
-    #print(type(index_min))
-    temp_path = list(perm[index_min])
-    temp_path.insert(0,source)
-    temp_path.append(target)
-    res = {'path': temp_path, 'distance': path_min}
-
-    return res
-
-
-
-
-
-
+        for j in range(len(temp) - 1):
+            # print(temp[j])
+            d = d + d_matrix[temp1.index(temp[j])][temp1.index(temp[j + 1])]
+            # print(temp1.index(temp[j]),temp1.index(temp[j+1]))
+            # print(d)
+        if d < min_dist:
+            min_dist = d
+            min_path = list(item)
+            m['path'] = min_path
+            m['distance'] = min_dist
