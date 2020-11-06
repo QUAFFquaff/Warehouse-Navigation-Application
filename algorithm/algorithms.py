50b2e4f33d65eb7ef08a9f3f199adc7118633ae0
zhengyb0301 <73626649+zhengyb0301@users.noreply.github.com>
2020-11-05 16:29:07 -0800

    add label

--- algorithm/algorithms.py
+++ algorithm/algorithms.py
@@ -39,6 +39,12 @@ def draw_png_graph(products,res_ind):
         G.add_edge(res[i-1],res[i])
     pos = nx.get_node_attributes(G,'pos')
     node_labels = nx.get_node_attributes(G, 'label')
+    # nx.draw(G, pos=pos,  node_size=500, labels=node_labels,
+    #         arrowstyle='->', arrows=True,
+    #         arrowsize=30, edge_color='red',
+    #         width=1
+    #         )
+    print("大傻逼")
     nx.draw(G, pos=pos,  node_size=500, labels=node_labels, font='bond',
             arrowstyle='->', arrows=True,
             arrowsize=30, edge_color='red',
@@ -49,17 +55,39 @@ def draw_png_graph(products,res_ind):
     pass
 
 def draw_png_dot_graph(products):
+    #import pdb
+    #pdb.set_trace()
     plt.figure(figsize=(9, 9))
+    #pdb.set_trace()
     G = nx.DiGraph()
+   # pdb.set_trace()
     for p in products:
+      #  pdb.set_trace()
         G.add_node(p[0],label = p[0], pos=(p[1],p[2]))
     pos = nx.get_node_attributes(G,'pos')
+    #pdb.set_trace()
     node_labels = nx.get_node_attributes(G, 'label')
+    #pdb.set_trace()
+    print("123")
+    # nx.draw(G, pos=pos,  node_size=500, labels=node_labels,
+    #         arrowstyle='->', arrows=True,
+    #         arrowsize=30, edge_color='red',
+    #         width=1
+    #         )
     nx.draw(G, pos=pos,  node_size=500, labels=node_labels, font='bond',
             arrowstyle='->', arrows=True,
             arrowsize=30, edge_color='red',
             width=1, directed=True
             )
+
+    # plt.show()
+    # G = nx.house_graph()
+    # pos = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (1, 1), 4: (0.5, 2.0)}
+    #
+    # nx.draw_networkx_nodes(G, pos, node_size=2000, nodelist=[4])
+    # nx.draw_networkx_nodes(G, pos, node_size=3000, nodelist=[0, 1, 2, 3], node_color="b")
+    # nx.draw_networkx_edges(G, pos, alpha=0.5, width=6)
+    #pdb.set_trace()
     plt.savefig("data/path/dot.png")
     print("generate: dot.png")
     pass
