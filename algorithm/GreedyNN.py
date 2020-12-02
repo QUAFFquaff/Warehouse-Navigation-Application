d5cfa8103d872a72cdaadb67d54d25d3a816f4bc
tyzhao0931 <tonyzhao_lz@outlook.com>
2020-12-02 14:25:27 -0800

    nn update0

--- algorithm/GreedyNN.py
+++ algorithm/GreedyNN.py
@@ -24,19 +24,11 @@ def greedy_nn(d_matrix, source, target, plist) -> dict:
         nnl_trim = [j for i, j in enumerate(nnl) if i not in pop_list]
 
         d = d + min(nnl_trim)
-        if min(nnl_trim) == 0:
 
-            id1 = [i for i, x in enumerate(nnl) if x == 0]
-            id2 = [y for j, y in enumerate(id1) if y not in pop_list]
-
-            path_list.append(temp[id2[0]])
-
-            prev = id2[0]
-        else:
-            id1 = [i for i, x in enumerate(nnl) if x == min(nnl_trim)]
-            id2 = [y for j, y in enumerate(id1) if y not in pop_list]
-            path_list.append(temp[id2[0]])
-            prev = id2[0]
+        id1 = [i for i, x in enumerate(nnl) if x == min(nnl_trim)]
+        id2 = [y for j, y in enumerate(id1) if y not in pop_list]
+        path_list.append(temp[id2[0]])
+        prev = id2[0]
         pop_list.append(prev)
 
     d = d + d_matrix[prev][len(temp) - 1]
