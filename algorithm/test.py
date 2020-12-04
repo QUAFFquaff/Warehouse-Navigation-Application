b1200159a35c12b2472a1ff6ff3b7417bb125b47
632633533@qq.com <quaff,lyu@gmail.com>
2020-12-05 01:15:55 +0800

    change pos

--- algorithm/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
-# @Time    : 11/25/2020 14:08
-# @Author  : Haoyu Lyu
-# @File    : test.py
-# @Software: PyCharm
-temp_d = {'a':1}
-print(temp_d.items())
